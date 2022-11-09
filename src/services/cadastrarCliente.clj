3e2d5a06e965bc50127b62ae8d7496997a8e2c16
Vinicius Rocha Pereira <vinicius.rocha.pereira.com>
2022-11-09 08:28:38 -0400

    :sparkles: feat: finalizando o cadastro de clientes

--- src/services/cadastrarCliente.clj
+++ src/services/cadastrarCliente.clj
@@ -1,7 +1,5 @@
 (ns services.cadastrarCliente (:gen-class)
     (:require [cassandra.config :refer [insert select]]))
 (defn cadastrar-cliente [data]
-;;So enviar para o inser um mapa que tenha somente as chaves com os mesmos nomes das colunas da tabela
-  (insert "nome-da-tabela", data)
-;;retornar o 'data' que veio de argumento;
-  )
+  (insert "tbl_clientes" (dissoc data :situacao))
+  (assoc data :situacao "processado"))
