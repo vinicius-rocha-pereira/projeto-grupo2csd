1c26d63c9b3f5497e9681c598c8b4de1cc203b7b
Vinicius Rocha Pereira <vinicius.rocha.pereira.com>
2022-11-09 07:38:52 -0400

    :sparkles: feat: adicionando o cadastro de usuario

--- src/services/cadastrarUsuario.clj
+++ src/services/cadastrarUsuario.clj
@@ -2,7 +2,6 @@
     (:require [cassandra.config :refer [insert select]])
     (:gen-class))
 (defn cadastrar-usuarios [data]
-;;So enviar para o inser um mapa que tenha somente as chaves com os mesmos nomes das colunas da tabela
-  (insert "nome-da-tabela", data)
-;;retornar o 'data' que veio de argumento;
-  )
+  (insert "tbl_usuarios" (dissoc data :situacao))
+  (assoc data :situacao "processado"))
+(defn validar-dados-usuario [data])
