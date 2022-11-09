4cd2ff34f5c11d0defb299c68b271233e2b14896
Vinicius Rocha Pereira <vinicius.rocha.pereira.com>
2022-11-09 08:26:09 -0400

    :sparkles: feat: finalizando o cadastro de usuarios

--- src/services/cadastrarUsuario.clj
+++ src/services/cadastrarUsuario.clj
@@ -4,4 +4,5 @@
 (defn cadastrar-usuarios [data]
   (insert "tbl_usuarios" (dissoc data :situacao))
   (assoc data :situacao "processado"))
-(defn validar-dados-usuario [data])
+
+;; {"cpf": "25465845500", "senha": "maria123", "bairro": "Mariana", "cidade": "São paulo", "complemento": "Apartamento", "contato": "11945621234", "data_nascimento": "1990-11-28", "email": "joao.rocha@oulook.com", "nome": "Jõao Rocha", "numero": "2524", "rua": "Mariana", "situacao":"pendente"}
